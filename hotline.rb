e34a171f14215f2dd701be1d1b8e23bc8b8c3df9
Alex Soble <asoble@gmail.com>
2025-03-03 08:22:47 -0600

    Add more script

--- hotline.rb
+++ hotline.rb
@@ -4,7 +4,9 @@ require 'twilio-ruby'
 post '/hotline' do
   content_type 'text/xml'
 
+  WELCOME_MESSAGE = 'Hello and welcome to the Water Department hotline.'
+
   Twilio::TwiML::VoiceResponse.new do | response |
-    response.say(message: "Hello World")
+    response.say(message: WELCOME_MESSAGE)
   end.to_s
 end
