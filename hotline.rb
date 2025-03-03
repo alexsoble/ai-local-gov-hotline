11ce3b5d14ddb301620527003d64567f672bfce7
Alex Soble <asoble@gmail.com>
2025-03-03 08:31:12 -0600

    Print params

--- hotline.rb
+++ hotline.rb
@@ -1,12 +1,24 @@
-require 'sinatra'
-require 'twilio-ruby'
+require "sinatra"
+require "twilio-ruby"
 
-post '/hotline' do
-  content_type 'text/xml'
+post "/hotline" do
+  content_type "text/xml"
 
-  WELCOME_MESSAGE = 'Hello and welcome to the Water Department hotline.'
+  WELCOME_MESSAGE = "Hello and welcome to the Water Department hotline. How may we assist you today?"
 
   Twilio::TwiML::VoiceResponse.new do | response |
-    response.say(message: WELCOME_MESSAGE)
+    response.say(
+      message: WELCOME_MESSAGE
+    )
+
+    response.gather(
+      input: "speech"
+      action: "https://guarded-atoll-40934-2505c0c0e70b.herokuapp.com/handle_speech"
+      method: "POST"
+    )
   end.to_s
+end
+
+post "/handle_speech" do   
+  puts(params)
 end
